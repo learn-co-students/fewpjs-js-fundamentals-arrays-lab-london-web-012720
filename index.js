7d5bce4c04a5f3b7bab6f3a3531b7ccb8b8aeb88
Bryn <thenazboz@gmail.com>
2020-03-09 11:37:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution here!
+let pets = ["Milo", "Otis", "Garfield"]
+ 
+let append = [...pets]
+append.push("Odie");
+
+
+ 
+let removeLast = [...pets]
+removeLast.pop( );
+
+ 
+ 
+let prepend = [...pets]
+prepend.unshift("Odie");
+
+ 
+ 
+let removeFirst = [...pets]
+removeFirst.shift();
+
+ 
